18576b1d293c51543c0d08f2f913092661bf7230
yas373 <yasin_turpcu_67@hotmail.com>
2018-04-05 22:54:22 +0300

    Initial Commit

---
+++ AgacOdevi/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AgacOdevi
+//
+//  Created by Yasin Turpcu on 5.04.2018.
+//  Copyright © 2018 Yasin Turpcu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
